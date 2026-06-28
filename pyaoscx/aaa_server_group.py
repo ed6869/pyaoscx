744db2ae1c7d853ddca7a5c4d73d81c4916037bf
ed6869 <ed6869@mail.com>
2026-06-28 21:03:58 +0200

    Add AAA server family resource modules
    Add three new pyaoscx resource modules for AAA configuration:
    
    - AaaServerGroup (system/aaa_server_groups): manage AAA server groups
      indexed by group_name with a configurable group_type.
    - RadiusServer (system/vrfs/{vrf}/radius_servers): manage RADIUS servers,
      VRF-nested with the compound index address,port,port_type and support
      for the server_group binding (REST v10.16).
    - TacacsServer (system/vrfs/{vrf}/tacacs_servers): manage TACACS+ servers,
      VRF-nested with the compound index address,tcp_port and group membership.
    
    All three classes are registered in the api module-name table and use a
    PUT-based update. Includes the v10.16 REST version module (required for the
    RADIUS server_group binding) and 17 offline unit tests in tests/test_aaa.py.
    
    Signed-off-by: ed6869 <ed6869@mail.com>

---
+++ pyaoscx/aaa_server_group.py
@@ -0,0 +1,270 @@
+# (C) Copyright 2019-2024 Hewlett Packard Enterprise Development LP.
+# Apache License 2.0
+
+import json
+import logging
+import re
+
+from pyaoscx.exceptions.generic_op_error import GenericOperationError
+from pyaoscx.exceptions.response_error import ResponseError
+
+from pyaoscx.utils import util as utils
+
+from pyaoscx.pyaoscx_module import PyaoscxModule
+
+
+class AaaServerGroup(PyaoscxModule):
+    """
+    Provide configuration management for AAA Server Groups on AOS-CX devices.
+    """
+
+    base_uri = "system/aaa_server_groups"
+    resource_uri_name = "aaa_server_groups"
+
+    indices = ["group_name"]
+
+    def __init__(self, session, group_name, uri=None, **kwargs):
+        self.session = session
+        self.group_name = group_name
+        self._uri = uri
+        self.config_attrs = []
+        self.materialized = False
+        self._original_attributes = {}
+        utils.set_creation_attrs(self, **kwargs)
+        self.__modified = False
+        self.path = "{0}/{1}".format(self.base_uri, self.group_name)
+
+    @PyaoscxModule.connected
+    def get(self, depth=None, selector=None):
+        """
+        Perform a GET call to retrieve data for an AAA Server Group and fill
+            the object with the incoming attributes.
+
+        :param depth: Integer deciding how many levels into the API JSON that
+            references will be returned.
+        :param selector: Alphanumeric option to select specific information to
+            return.
+        :return: Returns True if there is not an exception raised.
+        """
+        logging.info("Retrieving %s from switch", self)
+
+        depth = depth or self.session.api.default_depth
+        selector = selector or self.session.api.default_selector
+
+        if not self.session.api.valid_depth(depth):
+            depths = self.session.api.valid_depths
+            raise Exception("ERROR: Depth should be {0}".format(depths))
+
+        if selector not in self.session.api.valid_selectors:
+            selectors = " ".join(self.session.api.valid_selectors)
+            raise Exception(
+                "ERROR: Selector should be one of {0}".format(selectors)
+            )
+
+        payload = {"depth": depth, "selector": selector}
+
+        try:
+            response = self.session.request("GET", self.path, params=payload)
+        except Exception as e:
+            raise ResponseError("GET", e)
+
+        if not utils._response_ok(response, "GET"):
+            raise GenericOperationError(response.text, response.status_code)
+
+        data = json.loads(response.text)
+
+        if "group_name" in data:
+            data.pop("group_name")
+
+        utils.create_attrs(self, data)
+
+        if selector in self.session.api.configurable_selectors:
+            utils.set_config_attrs(self, data, "config_attrs", ["group_name"])
+
+        self._original_attributes = data
+
+        self.materialized = True
+        return True
+
+    @classmethod
+    def get_all(cls, session):
+        """
+        Perform a GET call to retrieve all AAA Server Groups, and create a
+            dictionary containing them.
+
+        :param cls: Object's class.
+        :param session: pyaoscx.Session object used to represent a logical
+            connection to the device.
+        :return: Dictionary containing the group names as keys and the
+            AaaServerGroup objects as values.
+        """
+        logging.info("Retrieving all %s data from switch", cls.__name__)
+
+        try:
+            response = session.request("GET", cls.base_uri)
+        except Exception as e:
+            raise ResponseError("GET", e)
+
+        if not utils._response_ok(response, "GET"):
+            raise GenericOperationError(response.text, response.status_code)
+
+        data = json.loads(response.text)
+
+        groups = {}
+        uri_list = session.api.get_uri_from_data(data)
+        for uri in uri_list:
+            indices, group = cls.from_uri(session, uri)
+            groups[indices] = group
+
+        return groups
+
+    @PyaoscxModule.connected
+    def apply(self):
+        """
+        Main method used to either create or update an existing AAA Server
+            Group.
+
+        :return: Boolean, True if object was created or modified.
+        """
+        if self.materialized:
+            modified = self.update()
+        else:
+            modified = self.create()
+        self.__modified = modified
+        return modified
+
+    @PyaoscxModule.connected
+    def update(self):
+        """
+        Perform a PUT call to apply changes to an existing AAA Server Group.
+
+        :return: True if Object was modified and a PUT request was made.
+        """
+        group_data = utils.get_attrs(self, self.config_attrs)
+
+        if group_data == self._original_attributes:
+            return False
+
+        post_data = json.dumps(group_data)
+
+        try:
+            response = self.session.request("PUT", self.path, data=post_data)
+        except Exception as e:
+            raise ResponseError("PUT", e)
+
+        if not utils._response_ok(response, "PUT"):
+            raise GenericOperationError(response.text, response.status_code)
+
+        logging.info("SUCCESS: Updating %s", self)
+        self._original_attributes = group_data
+        return True
+
+    @PyaoscxModule.connected
+    def create(self):
+        """
+        Perform a POST call to create a new AAA Server Group. Only returns if
+            no exception is raised.
+
+        :return: Boolean, True if entry was created.
+        """
+        group_data = utils.get_attrs(self, self.config_attrs)
+        group_data["group_name"] = self.group_name
+
+        post_data = json.dumps(group_data)
+
+        try:
+            response = self.session.request(
+                "POST", self.base_uri, data=post_data
+            )
+        except Exception as e:
+            raise ResponseError("POST", e)
+
+        if not utils._response_ok(response, "POST"):
+            raise GenericOperationError(response.text, response.status_code)
+
+        logging.info("SUCCESS: Adding %s", self)
+
+        self.get()
+        return True
+
+    @PyaoscxModule.connected
+    def delete(self):
+        """
+        Perform DELETE call to delete an AAA Server Group.
+        """
+        try:
+            response = self.session.request("DELETE", self.path)
+        except Exception as e:
+            raise ResponseError("DELETE", e)
+
+        if not utils._response_ok(response, "DELETE"):
+            raise GenericOperationError(response.text, response.status_code)
+
+        logging.info("SUCCESS: Deleting %s", self)
+        utils.delete_attrs(self, self.config_attrs)
+
+    @classmethod
+    def from_response(cls, session, response_data):
+        """
+        Create an AaaServerGroup object given a response_data.
+
+        :param cls: Class calling the method.
+        :param session: pyaoscx.Session object.
+        :param response_data: The response must be a dictionary of the form:
+            {group_name: "/rest/v10.xx/system/aaa_server_groups/group_name"}
+        :return: AaaServerGroup object.
+        """
+        group_arr = session.api.get_keys(response_data, cls.resource_uri_name)
+        group_name = group_arr[0]
+        return cls(session, group_name)
+
+    @classmethod
+    def from_uri(cls, session, uri):
+        """
+        Create an AaaServerGroup object given a URI.
+
+        :param cls: Class calling the method.
+        :param session: pyaoscx.Session object.
+        :param uri: a String with a URI.
+        :return: tuple containing both the index and the AaaServerGroup object.
+        """
+        index_pattern = re.compile(r"(.*)aaa_server_groups/(?P<index>.+)")
+        group_name = index_pattern.match(uri).group("index")
+
+        group = cls(session, group_name)
+        return group_name, group
+
+    def __str__(self):
+        return "AaaServerGroup group_name:{0}".format(self.group_name)
+
+    @PyaoscxModule.deprecated
+    def get_uri(self):
+        """
+        Method used to obtain the specific AAA Server Group URI.
+
+        :return: Object's URI.
+        """
+        if self._uri is None:
+            self._uri = "{0}{1}/{2}".format(
+                self.session.resource_prefix,
+                self.base_uri,
+                self.group_name,
+            )
+        return self._uri
+
+    @PyaoscxModule.deprecated
+    def get_info_format(self):
+        """
+        Method used to obtain correct object format for referencing inside
+            other objects.
+
+        :return: Object format depending on the API Version.
+        """
+        return self.session.api.get_index(self)
+
+    @property
+    def modified(self):
+        """
+        Return boolean with whether this object has been modified.
+        """
+        return self.__modified
---
+++ pyaoscx/radius_server.py
@@ -0,0 +1,288 @@
+# (C) Copyright 2019-2024 Hewlett Packard Enterprise Development LP.
+# Apache License 2.0
+
+import json
+import logging
+import re
+
+from pyaoscx.exceptions.generic_op_error import GenericOperationError
+from pyaoscx.exceptions.response_error import ResponseError
+
+from pyaoscx.utils import util as utils
+
+from pyaoscx.pyaoscx_module import PyaoscxModule
+
+
+class RadiusServer(PyaoscxModule):
+    """
+    Provide configuration management for RADIUS servers on AOS-CX devices.
+        RADIUS servers are nested under a VRF and indexed by the compound key
+        address, port and port_type.
+    """
+
+    collection_uri = "system/vrfs/{name}/radius_servers"
+    resource_uri_name = "radius_servers"
+
+    indices = ["address", "port", "port_type"]
+
+    def __init__(
+        self,
+        session,
+        vrf,
+        address,
+        port=1812,
+        port_type="udp",
+        uri=None,
+        **kwargs
+    ):
+        self.session = session
+        # vrf is a Vrf object used as the parent in the URI path
+        self.vrf = vrf
+        self.address = address
+        self.port = port
+        self.port_type = port_type
+        self._uri = uri
+        self.config_attrs = []
+        self.materialized = False
+        self._original_attributes = {}
+        utils.set_creation_attrs(self, **kwargs)
+        self.__modified = False
+        self.base_uri = self.collection_uri.format(name=self.vrf.name)
+        self.path = "{0}/{1}".format(self.base_uri, self._index_id())
+
+    def _index_id(self):
+        sep = self.session.api.compound_index_separator
+        return sep.join(
+            [str(self.address), str(self.port), str(self.port_type)]
+        )
+
+    @PyaoscxModule.connected
+    def get(self, depth=None, selector=None):
+        """
+        Perform a GET call to retrieve data for a RADIUS server and fill the
+            object with the incoming attributes.
+
+        :param depth: Integer deciding how many levels into the API JSON that
+            references will be returned.
+        :param selector: Alphanumeric option to select specific information to
+            return.
+        :return: Returns True if there is not an exception raised.
+        """
+        logging.info("Retrieving %s from switch", self)
+
+        depth = depth or self.session.api.default_depth
+        selector = selector or self.session.api.default_selector
+
+        if not self.session.api.valid_depth(depth):
+            depths = self.session.api.valid_depths
+            raise Exception("ERROR: Depth should be {0}".format(depths))
+
+        if selector not in self.session.api.valid_selectors:
+            selectors = " ".join(self.session.api.valid_selectors)
+            raise Exception(
+                "ERROR: Selector should be one of {0}".format(selectors)
+            )
+
+        payload = {"depth": depth, "selector": selector}
+
+        try:
+            response = self.session.request("GET", self.path, params=payload)
+        except Exception as e:
+            raise ResponseError("GET", e)
+
+        if not utils._response_ok(response, "GET"):
+            raise GenericOperationError(response.text, response.status_code)
+
+        data = json.loads(response.text)
+
+        for index in self.indices + ["vrf"]:
+            if index in data:
+                data.pop(index)
+
+        utils.create_attrs(self, data)
+
+        if selector in self.session.api.configurable_selectors:
+            utils.set_config_attrs(
+                self, data, "config_attrs", self.indices + ["vrf"]
+            )
+
+        self._original_attributes = data
+
+        self.materialized = True
+        return True
+
+    @classmethod
+    def get_all(cls, session, vrf):
+        """
+        Perform a GET call to retrieve all RADIUS servers under a VRF, and
+            create a dictionary containing them.
+
+        :param cls: Object's class.
+        :param session: pyaoscx.Session object.
+        :param vrf: Vrf object the RADIUS servers belong to.
+        :return: Dictionary containing the compound indices as keys and the
+            RadiusServer objects as values.
+        """
+        logging.info("Retrieving all %s data from switch", cls.__name__)
+
+        uri = cls.collection_uri.format(name=vrf.name)
+        try:
+            response = session.request("GET", uri)
+        except Exception as e:
+            raise ResponseError("GET", e)
+
+        if not utils._response_ok(response, "GET"):
+            raise GenericOperationError(response.text, response.status_code)
+
+        data = json.loads(response.text)
+
+        servers = {}
+        uri_list = session.api.get_uri_from_data(data)
+        for server_uri in uri_list:
+            indices, server = cls.from_uri(session, vrf, server_uri)
+            servers[indices] = server
+
+        return servers
+
+    @PyaoscxModule.connected
+    def apply(self):
+        """
+        Main method used to either create or update an existing RADIUS server.
+
+        :return: Boolean, True if object was created or modified.
+        """
+        if self.materialized:
+            modified = self.update()
+        else:
+            modified = self.create()
+        self.__modified = modified
+        return modified
+
+    @PyaoscxModule.connected
+    def update(self):
+        """
+        Perform a PUT call to apply changes to an existing RADIUS server.
+
+        :return: True if Object was modified and a PUT request was made.
+        """
+        server_data = utils.get_attrs(self, self.config_attrs)
+
+        if server_data == self._original_attributes:
+            return False
+
+        post_data = json.dumps(server_data)
+
+        try:
+            response = self.session.request("PUT", self.path, data=post_data)
+        except Exception as e:
+            raise ResponseError("PUT", e)
+
+        if not utils._response_ok(response, "PUT"):
+            raise GenericOperationError(response.text, response.status_code)
+
+        logging.info("SUCCESS: Updating %s", self)
+        self._original_attributes = server_data
+        return True
+
+    @PyaoscxModule.connected
+    def create(self):
+        """
+        Perform a POST call to create a new RADIUS server. Only returns if no
+            exception is raised.
+
+        :return: Boolean, True if entry was created.
+        """
+        server_data = utils.get_attrs(self, self.config_attrs)
+        server_data["address"] = self.address
+        server_data["port"] = self.port
+        server_data["port_type"] = self.port_type
+        server_data["vrf"] = self.vrf.get_uri()
+
+        post_data = json.dumps(server_data)
+
+        try:
+            response = self.session.request(
+                "POST", self.base_uri, data=post_data
+            )
+        except Exception as e:
+            raise ResponseError("POST", e)
+
+        if not utils._response_ok(response, "POST"):
+            raise GenericOperationError(response.text, response.status_code)
+
+        logging.info("SUCCESS: Adding %s", self)
+
+        self.get()
+        return True
+
+    @PyaoscxModule.connected
+    def delete(self):
+        """
+        Perform DELETE call to delete a RADIUS server.
+        """
+        try:
+            response = self.session.request("DELETE", self.path)
+        except Exception as e:
+            raise ResponseError("DELETE", e)
+
+        if not utils._response_ok(response, "DELETE"):
+            raise GenericOperationError(response.text, response.status_code)
+
+        logging.info("SUCCESS: Deleting %s", self)
+        utils.delete_attrs(self, self.config_attrs)
+
+    @classmethod
+    def from_uri(cls, session, vrf, uri):
+        """
+        Create a RadiusServer object given a URI.
+
+        :param cls: Class calling the method.
+        :param session: pyaoscx.Session object.
+        :param vrf: Vrf object the RADIUS server belongs to.
+        :param uri: a String with a URI.
+        :return: tuple containing both the compound index and the RadiusServer
+            object.
+        """
+        index_pattern = re.compile(r"(.*)radius_servers/(?P<index>.+)")
+        index = index_pattern.match(uri).group("index")
+        address, port, port_type = index.split(
+            session.api.compound_index_separator
+        )
+
+        server = cls(session, vrf, address, int(port), port_type)
+        return index, server
+
+    def __str__(self):
+        return "RadiusServer address:{0}, port:{1}, port_type:{2}".format(
+            self.address, self.port, self.port_type
+        )
+
+    @PyaoscxModule.deprecated
+    def get_uri(self):
+        """
+        Method used to obtain the specific RADIUS server URI.
+
+        :return: Object's URI.
+        """
+        if self._uri is None:
+            self._uri = "{0}{1}".format(
+                self.session.resource_prefix, self.path
+            )
+        return self._uri
+
+    @PyaoscxModule.deprecated
+    def get_info_format(self):
+        """
+        Method used to obtain correct object format for referencing inside
+            other objects.
+
+        :return: Object format depending on the API Version.
+        """
+        return self.session.api.get_index(self)
+
+    @property
+    def modified(self):
+        """
+        Return boolean with whether this object has been modified.
+        """
+        return self.__modified
